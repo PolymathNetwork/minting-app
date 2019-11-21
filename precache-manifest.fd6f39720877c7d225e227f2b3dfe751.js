1a47c8ba67c5fda3c7c3a4e438c829e77674f2f7
Remon Nashid <remon.sherin@gmail.com>
2019-11-21 09:02:38 -0800

    Updates

---
+++ precache-manifest.fd6f39720877c7d225e227f2b3dfe751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5ecf7a0adf211e8c221a6e392a86f3",
+    "url": "/minting-app/index.html"
+  },
+  {
+    "revision": "c6073379affbeef7edc3",
+    "url": "/minting-app/static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "73698f0cb73795623a26",
+    "url": "/minting-app/static/css/main.3b9fece1.chunk.css"
+  },
+  {
+    "revision": "c6073379affbeef7edc3",
+    "url": "/minting-app/static/js/2.39735127.chunk.js"
+  },
+  {
+    "revision": "73698f0cb73795623a26",
+    "url": "/minting-app/static/js/main.04671e84.chunk.js"
+  },
+  {
+    "revision": "f4686b6e40cc637d1e11",
+    "url": "/minting-app/static/js/runtime~main.6dc462d5.js"
+  }
+]);
